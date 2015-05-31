8c128ebb5bd1ce4facc8a32bc8742c3efb2f6c44
Benjamin Cheng <ben@bcheng.cf>
2015-05-30 21:59:50 -0400

    A bit more mobile friendly now

---
+++ js/skelinit.js
@@ -0,0 +1,14 @@
+skel.init({
+    containers: '100%',
+    breakpoints: {
+        medium: {
+            media: "(min-width: 769px) and (max-width: 1024px)"
+        },
+        small: {
+            media: "(max-width: 768px)"
+        },
+        xsmall: {
+            media: "(max-width: 480px)"
+        }
+    }
+});
